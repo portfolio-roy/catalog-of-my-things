653f24e7ab68818d42ad408ad46e243533198a36
tsohleDev <mokhemisitsohle@gmail.com>
2023-03-28 16:21:52 +0200

    Update parameters for items constructor

--- item.rb
+++ item.rb
@@ -3,14 +3,17 @@ require 'date'
 class Item
   attr_accessor :id, :genre, :author, :source, :label, :publish_date, :archived
 
-  def initialize(id, genre, author, source, label, publish_date, archived)
-    @id = id
-    @genre = genre
-    @author = author
-    @source = source
-    @label = label
-    @publish_date = publish_date
-    @archived = archived
+  # Use a hash for the parameter
+  # Example: hash = {id => 1, genre> "Science", author> "Asif", source> "https://www.google.com", label=> "Science", publish_date=> "2020-01-01", archived=> false}
+  
+  def initialize(hash)
+    @id = hash.id
+    @genre = hash.genre
+    @author = hash.author
+    @source = hash.source
+    @label = hash.label
+    @publish_date = hash.publish_date
+    @archived = hash.archived
   end
 
   def can_be_archived?
