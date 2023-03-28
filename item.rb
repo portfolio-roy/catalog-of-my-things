84f64b6ff0ebe93571a0c3528a8ab48aa2d11b6f
tsohleDev <mokhemisitsohle@gmail.com>
2023-03-28 14:41:39 +0200

    Add method to check if archived

--- item.rb
+++ item.rb
@@ -1,24 +1,23 @@
 require 'date'
 
 class Item
-    attr_accessor :id, :genre, :author, :source, :label, :publish_date, :archived
+  attr_accessor :id, :genre, :author, :source, :label, :publish_date, :archived
 
-    def initialize(id, genre, author, source, label, publish_date, archived)
-        @id = id
-        @genre = genre
-        @author = author
-        @source = source
-        @label = label
-        @publish_date = publish_date
-        @archived = archived
-    end
+  def initialize(id, genre, author, source, label, publish_date, archived)
+    @id = id
+    @genre = genre
+    @author = author
+    @source = source
+    @label = label
+    @publish_date = publish_date
+    @archived = archived
+  end
 
-    # Toshle: add your code here
-    # define Methode can_be_archived?
-    # logic: current year - publish_date.year > 10
-    
-    # Asif: add your code here
-    # define Methode move_to_archive
-    # logic: @archived = can_be_archived?
+  def can_be_archived?
+    (Time.now.year - @publish_date.year) > 10
+  end
 
+  # Asif: add your code here
+  # define Methode move_to_archive
+  # logic: @archived = can_be_archived?
 end
