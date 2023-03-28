09ff96d5cba8ad5415c5a38ce4a9d2230f08087e
Utsargo Roy <royutsargo@gmail.com>
2023-03-28 16:12:11 +0600

    Add item class: - Create Item class in item.rb file - Setup linter

---
+++ item.rb
@@ -0,0 +1,24 @@
+require 'date'
+
+class Item
+    attr_accessor :id, :genre, :author, :source, :label, :publish_date, :archived
+
+    def initialize(id, genre, author, source, label, publish_date, archived)
+        @id = id
+        @genre = genre
+        @author = author
+        @source = source
+        @label = label
+        @publish_date = publish_date
+        @archived = archived
+    end
+
+    # Toshle: add your code here
+    # define Methode can_be_archived?
+    # logic: current year - publish_date.year > 10
+    
+    # Asif: add your code here
+    # define Methode move_to_archive
+    # logic: @archived = can_be_archived?
+
+end
