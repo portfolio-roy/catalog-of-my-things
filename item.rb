a11e8a7464a3b5f83a067c44cc4391db52dd414f
tsohleDev <mokhemisitsohle@gmail.com>
2023-03-29 12:03:46 +0200

    Fix linters error

--- item.rb
+++ item.rb
@@ -6,7 +6,6 @@ class Item
   # Use a hash for the parameter
   # Example: hash = {id => 1, genre> "Science", author> "Asif", source> "https://www.google.com", label=> "Science", publish_date=> "2020-01-01", archived=> false}
 
-
   def initialize(hash)
     # create a random id using time
     @id = hash['id'] || Time.now.to_i
@@ -16,6 +15,10 @@ class Item
     @label = hash['label']
     @publish_date = hash['publish_date']
     @archived = hash['archived']
+  end
+
+  def add_genre(genre)
+    @genre = genre
   end
 
   def can_be_archived?
