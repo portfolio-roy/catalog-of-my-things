fd2df0a15092415f1b7028058fd7c7f4d15cd767
tsohleDev <mokhemisitsohle@gmail.com>
2023-03-29 10:03:35 +0200

    Merge from items

--- item.rb
+++ item.rb
@@ -6,7 +6,6 @@ class Item
   # Use a hash for the parameter
   # Example: hash = {id => 1, genre> "Science", author> "Asif", source> "https://www.google.com", label=> "Science", publish_date=> "2020-01-01", archived=> false}
 
-
   def initialize(hash)
     # create a random id using time
     @id = hash['id'] || Time.now.to_i
