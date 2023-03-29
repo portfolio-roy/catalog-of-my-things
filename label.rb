516f56b2e4c4228dbbd98799c98afa83708d47e5
Utsargo Roy <royutsargo@gmail.com>
2023-03-29 21:11:16 +0600

    Fix linter errors

--- label.rb
+++ label.rb
@@ -4,7 +4,7 @@ class Label < Item
   attr_accessor :title, :color
   attr_reader :id, :items
 
-  def initialize(dictionary = {}, title, color, id: Random.rand(1..1000))
+  def initialize(title, color, dictionary = {}, id: Random.rand(1..1000))
     super(dictionary)
     @id = id
     @title = title
