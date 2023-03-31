33799fcc54bdabd5f47b101fe8f364bc84e5d55d
asifaminisonline <asifaminisonline@gmail.com>
2023-04-01 05:12:40 +0530

    Write unit tests for Game and Author class methods

--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -14,4 +14,20 @@ describe 'Unit testing for Author Class' do
       expect(@author).not_to be_instance_of Item
     end
 
-    
+    it 'should be a kind of Item' do
+      expect(@author).to be_kind_of Item
+    end
+  end
+
+  context 'First name of author' do
+    it 'should be "Oluwatoyin"' do
+      expect(@author.first_name).to eql 'Oluwatoyin'
+    end
+  end
+
+  context 'Last name of author' do
+    it 'should be "Olaoye"' do
+      expect(@author.last_name).to eql 'Olaoye'
+    end
+  end
+end
